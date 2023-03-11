b089096e5df819729e59c871afd99f588f62f266
Katerina <smilevskakaterina5@gmail.com>
2023-03-11 19:07:01 +0100

    List of favourite products for each user added.
    # Conflicts:
    #	src/main/java/com/example/product_aggregator_project/service/impl/UserServiceImpl.java
    #	src/main/java/com/example/product_aggregator_project/web/UserController.java

--- src/main/java/com/example/product_aggregator_project/web/UserFavouriteController.java
+++ src/main/java/com/example/product_aggregator_project/web/UserFavouriteController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
